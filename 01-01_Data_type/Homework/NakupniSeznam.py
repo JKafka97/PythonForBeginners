e865b0331231f180b977b729e83671a3a63ad821
Jaroslav Kafka <jaroslav.kafka@elso.systems.com>
2022-01-16 14:05:42 +0100

    Přidání úkolů

---
+++ 01-01_Data_type/Homework/NakupniSeznam.py
@@ -0,0 +1,1 @@
+#lečo (paprika, rajče, žádný vkus) boloňka (rajče, maso, dobrý vkus) topinka (maso, paprika, vkus tak na půl)
---
+++ 01-01_Data_type/Homework/obsahCtverceFormated.py
@@ -0,0 +1,1 @@
+#zeptej se uživatele na parametry a vypočítej mu obsah čtvrce i s odpovědí (použíj .Format())
