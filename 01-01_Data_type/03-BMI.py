d13f07a6a31a33a3bdb5d098ffbea2b5886e2e59
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-17 19:11:47 +0100

    Hotovo

--- 01-01_Data_type/03-BMI.py
+++ 01-01_Data_type/03-BMI.py
@@ -1,1 +1,8 @@
 #vytvoř BMI kalkulačku 
+
+# vaha = int(input('Kolik vážíš špeku? '))
+# vyska = float(input('Kolik měříš v metrech? '))
+# print('Tvoje BMI je:', vaha / (vyska * vyska))
+
+x = 1.5
+print(int(x))
