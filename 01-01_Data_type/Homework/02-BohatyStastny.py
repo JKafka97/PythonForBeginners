f640538ce9e966710eb4a942ddbbb4ebffd49188
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-18 18:55:24 +0100

    Hotovo

--- 01-01_Data_type/Homework/02-BohatyStastny.py
+++ 01-01_Data_type/Homework/02-BohatyStastny.py
@@ -1,5 +1,14 @@
 #štěstí == bohatství ?
 
-odpoved_stastny = input('Jsi štastný?')
-odpoved_bohaty = input('Jsi bohatý?')
-if odpoved_stastny == 'ano' and odpoved_bohaty ==
+stastny = input('Jsi štastný? ')
+bohaty = input('Jsi bohatý? ')
+if stastny == 'ano' and bohaty == 'ano':
+    print('Tak to jsi štastný člověk')
+elif stastny == 'ano' and bohaty == 'ne':
+    print('Najdi si lépe placenou práci.')
+elif stastny == 'ne' and bohaty == 'ano':
+    print('Kup si štěně. To tě rozveselí.')
+elif stastny == 'ne' and bohaty == 'ne':
+    print('Blbý.')
+else:
+    print('Máš odpovídat ano/ne, hlupáku.')
