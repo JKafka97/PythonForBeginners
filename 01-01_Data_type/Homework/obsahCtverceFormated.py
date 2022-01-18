12dd36e0bde86ac6e4ddc410efdfa3a1c82171ab
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-18 20:23:40 +0100

    Hotovo (i když mě to stálo pár let života)

--- 01-01_Data_type/Homework/obsahCtverceFormated.py
+++ 01-01_Data_type/Homework/obsahCtverceFormated.py
@@ -1,1 +1,5 @@
 #zeptej se uživatele na parametry a vypočítej mu obsah čtvrce i s odpovědí (použíj .Format())
+
+strana = int(input('Zadej délku strany v centimetrech: '))
+odpoved = 'Obsah čtverce se stranou {velikost} cm je {vypocet} cm2'
+print(odpoved.format(velikost = strana, vypocet = strana * strana))
