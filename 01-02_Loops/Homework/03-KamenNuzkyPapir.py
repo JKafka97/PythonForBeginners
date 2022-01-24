2ae7b7abad332a4bfdb2d6ad1114862e533abcf0
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-24 22:22:55 +0100

    Přidání podmínek do 'if' a 'elif'

--- 01-02_Loops/Homework/03-KamenNuzkyPapir.py
+++ 01-02_Loops/Homework/03-KamenNuzkyPapir.py
@@ -3,28 +3,29 @@
 from random import choice
 
 seznam_moznosti = ['kámen', 'nůžky', 'papír']
+
 def main():
     tip = tip_stroje()
-    while True:
-        otazka_uzivateli()                        #všechny def napsat sem
+    while True:    
+        otazka_uzivateli()
+        tip_stroje()
+        porovnani(odpoved_uzivatele, tip_stroje)                      #všechny def napsat sem
 
 def otazka_uzivateli():
-    return input('Naval odpoveď. ')
+    return input('Naval odpoveď. ') 
 
 def porovnani(odpoved_uzivatele, tip_stroje):
+    odpoved_uzivatele = otazka_uzivateli()
     if odpoved_uzivatele == tip_stroje:
-        print('Ještě jednou.')
-    elif odpoved_uzivatele == 'kámen' and tip_stroje == 'papír':
+        print('Plichta, ještě jednou.')
+    elif odpoved_uzivatele == 'kámen' and tip_stroje == 'papír' or odpoved_uzivatele == 'papír' and tip_stroje == 'nůžky' or odpoved_uzivatele== 'nůžky' and tip_stroje == 'kámen':
         print('Prohrál jsi.')
-    
+    elif odpoved_uzivatele == 'kámen' and tip_stroje == 'nůžky'or odpoved_uzivatele == 'papír' and tip_stroje == 'kámen' or odpoved_uzivatele == 'nůžky' and tip_stroje == 'papír':
+        print('Vyhrál jsi.')
+     
 
 def tip_stroje():
     return choice(seznam_moznosti)
 
 
 main()
-
-
-
-
-# dokončit doma
