168723709e33f56576f520790e017776f0a32b05
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-24 18:16:47 +0100

    Nadefinování scriptu pro cestu do lesa

--- 01-02_Loops/01-CestaDoLesa.py
+++ 01-02_Loops/01-CestaDoLesa.py
@@ -1,1 +1,10 @@
 #pomocí vlastních metod nadefinuj kroky na cestu do lesa (def + while)
+pocet_kroku = 0
+
+def cesta_do_lesa(pocet_kroku):
+    while pocet_kroku < 10:
+        print('Ušel jsem', pocet_kroku, 'kroky rovně.')
+        pocet_kroku += 1
+    
+
+cesta_do_lesa(pocet_kroku)
