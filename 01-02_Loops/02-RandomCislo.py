34b126b5c10b3d361b96a216876b0635be2d0199
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-24 18:44:54 +0100

    Vytvoření hry pro tip čísla

--- 01-02_Loops/02-RandomCislo.py
+++ 01-02_Loops/02-RandomCislo.py
@@ -1,1 +1,20 @@
 #Typni si číslo od 1 do 5! (while + random)
+#hra si jednou tipne číslo, opakovaně se na číslo bude ptát
+
+from random import randint
+
+def tipni_cislo():
+    return randint(1, 5)
+    
+def hra():
+    tip = tipni_cislo()
+    while True:
+        jeho_tip = int(input('Jaký je tip na číslo? '))
+        if jeho_tip == tip:
+            print('Dobrá práce.')
+            break
+        else:
+            print('Smůla hlupáku.')
+
+hra()  
+    
