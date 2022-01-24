13c72d812f884bae54517b27c166a401f8848054
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-24 19:55:44 +0100

    Oprava názvu

---
+++ 01-02_Loops/Homework/OpacnaKalkulacka.py
@@ -0,0 +1,9 @@
+#vyrob kalkulacku, která každé číslo vypíše zrcadlove (123 => 321) (index + while)
+
+
+
+tvoje_mama = 'fakt velkej string'
+
+print(tvoje_mama[2])
+print(tvoje_mama[2:4])                  #počáteční 2, poslední člen se nepočítá
+print(tvoje_mama[2:8:2])                # třetí číslo je krok o kolik se to posune
