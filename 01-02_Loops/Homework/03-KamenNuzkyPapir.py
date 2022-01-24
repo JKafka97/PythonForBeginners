2e0c747127e70823a7780a2fe6de1c612cce593a
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-24 19:18:19 +0100

    Přesunutí do HW

---
+++ 01-02_Loops/Homework/03-KamenNuzkyPapir.py
@@ -0,0 +1,30 @@
+# pomocí listu udělej kámen nužky papir (list + while)
+
+from random import choice
+
+seznam_moznosti = ['kámen', 'nůžky', 'papír']
+def main():
+    tip = tip_stroje()
+    while True:
+        otazka_uzivateli()                        #všechny def napsat sem
+
+def otazka_uzivateli():
+    return input('Naval odpoveď. ')
+
+def porovnani(odpoved_uzivatele, tip_stroje):
+    if odpoved_uzivatele == tip_stroje:
+        print('Ještě jednou.')
+    elif odpoved_uzivatele == 'kámen' and tip_stroje == 'papír':
+        print('Prohrál jsi.')
+    
+
+def tip_stroje():
+    return choice(seznam_moznosti)
+
+
+main()
+
+
+
+
+# dokončit doma
