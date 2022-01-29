caa77f5166edf9e11646d5db250ae7b03d92941c
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-29 19:08:03 +0100

    Přidání definic a podmínek

--- 01-02_Loops/Homework/OkoBere.py
+++ 01-02_Loops/Homework/OkoBere.py
@@ -6,3 +6,45 @@
 # když víc => vypíšu o kolik a že prohrál
 
 #(while + random)
+
+from random import randrange
+
+def main():
+    cislo_kasino()
+    cislo_uzivatel()
+    vypocet()
+
+def cislo_kasino():
+    return randrange(2, 10)
+
+def cislo_uzivatel():
+    return randrange(2, 10)
+
+def vypocet():
+    kasino = cislo_kasino()
+    uzivatel = cislo_uzivatel()
+    while uzivatel < 21:
+        dalsi_karta = input('Chceš otočit kartu? ')
+        if dalsi_karta == 'ano':
+            print("Máš", uzivatel, "bodů.")
+            print("Krupiér má", kasino, "bodů")
+            cislo_uzivatel()
+            cislo_kasino()
+            uzivatel = uzivatel + cislo_uzivatel()
+            kasino = kasino + cislo_kasino()
+            if kasino == 21:
+                print('Prohrál jsi')
+                break
+        elif dalsi_karta == 'ne':
+            break
+        else:
+            print('Odpovídej ano/ne')
+    if uzivatel == 21:
+        print('21, Black Jack. Vyhráváš!')
+    elif uzivatel > 21:
+        print('Prohrál jsi. Máš', uzivatel,'bodů')
+    else:
+        print('Chybělo ti', 21- uzivatel, 'bodů')
+
+main()
+
