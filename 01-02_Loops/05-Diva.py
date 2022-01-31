3eb7087656ad3aef4d8ad269a97025cd42c2273b
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-31 18:58:23 +0100

    Lekce probrána

--- 01-02_Loops/05-Diva.py
+++ 01-02_Loops/05-Diva.py
@@ -1,1 +1,12 @@
 # vypismenkuj všechna slova od uživatele (for + list)
+
+def main():
+    odpoved = pozadavek()
+    for i in odpoved:           # procházecí cyklus, projde všechny prvky té dané věci
+        print(i+ 'a')
+
+def pozadavek():
+    return input('Co chceš? ')
+
+
+main()
