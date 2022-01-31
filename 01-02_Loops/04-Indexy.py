b03fab0f673b44c01c839c4608ed49a8a9f2533b
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-01-31 18:59:51 +0100

    Lekce probrána- kod hotov

--- 01-02_Loops/04-Indexy.py
+++ 01-02_Loops/04-Indexy.py
@@ -1,1 +1,7 @@
 #Bodove hodnoceni lidí (dic + index)
+
+lidi_body = {"Jaroušek" : 5, "Domča" : 10, "Sabča" : 9, "Sára" : 5, "Amy" : 10}
+
+print(lidi_body["Domča"])
+lidi_body["Peggy"] = 4
+print(lidi_body)
