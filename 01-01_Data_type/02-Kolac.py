e78474d19f2681e3ebd6a42a6303484fdb4a405e
Erik Bršlica <erik.brslica@gmail.com>
2022-02-01 18:56:13 +0100

    Výpočet sežrání koláče

--- 01-01_Data_type/02-Kolac.py
+++ 01-01_Data_type/02-Kolac.py
@@ -2,3 +2,21 @@
 
 # rozdíl if a elif
 
+kolac = int(input("kolik procent kolace chces sezrat"))
+
+if kolac < 15:
+    print("to je v pohodě")
+
+elif kolac < 50:
+    print("tak akorát")
+
+
+elif kolac < 70:
+    print("to už je dost")
+
+
+elif kolac < 100:
+    print("prase")
+
+else:
+    print("nechápu")
