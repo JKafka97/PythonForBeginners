76f4e2c7d720e77e535b1344fff15face21379cc
Erik Bršlica <erik.brslica@gmail.com>
2022-02-01 19:26:48 +0100

    Výpočet princeznovského ideálu

--- 01-01_Data_type/05-Princezna.py
+++ 01-01_Data_type/05-Princezna.py
@@ -1,1 +1,16 @@
 # vymysli kombinace pro hezkeho/ošklivého/hloupého/chytrého
+
+krasa = input("je princezna hezká (ano/ne)? ")
+chytrost = input("je princezna chytrá (ano/ne)? ")
+
+if krasa == "ano" and chytrost == "ano":
+    print("dobrá partie")
+
+if krasa == "ano" and chytrost == "ne":
+    print("hezká blbka")
+
+if krasa == "ne" and chytrost == "ano":
+    print("Ošklivka Betty")
+
+if krasa == "ne" and chytrost == "ne":
+    print("ztracený případ")
