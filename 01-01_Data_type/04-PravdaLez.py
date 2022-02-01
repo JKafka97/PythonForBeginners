d3a814d21aea702762d109b9852e40a05f44f686
Erik Bršlica <erik.brslica@gmail.com>
2022-02-01 19:16:10 +0100

    True or/and False

--- 01-01_Data_type/04-PravdaLez.py
+++ 01-01_Data_type/04-PravdaLez.py
@@ -1,14 +1,14 @@
 #kdy bude pravda a kdy lež
 
-x = True and True   # 
-y = True and False  # 
-z = False and True  # 
-v = False and False # 
+x = True and True   # 1
+y = True and False  # 0
+z = False and True  # 0
+v = False and False # 0
 
-c = True or True    # 
-a = True or False   # 
-b = False or True   # 
-e = False or False  # 
-h = True and False or True # 
+c = True or True    # 1
+a = True or False   # 1
+b = False or True   # 1
+e = False or False  # 0
+h = True and False or True # 1
 
 print(x, y, z, v, c, a ,b, e, h)
