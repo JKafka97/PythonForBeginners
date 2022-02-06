fa70a95e019e99ff1e1901ee2610319047ce59cd
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-02-06 18:49:49 +0100

    Udělání def, hrubý nástřel kodu

--- 01-02_Loops/Homework/Kviz.py
+++ 01-02_Loops/Homework/Kviz.py
@@ -7,4 +7,23 @@
 
 from random import shuffle
 
-hra = {""}    # stejně jako je v indexu 
+print('Vítej v kvízu o seriálu Simpsons')
+
+hra = {"žlutou" : "Všechny postavičky v tomto seriálu mají zvláštní barvu kůže. Jakou?",
+        "saxofon" : "Líza ráda hraje na jeden hudební nástroj. Na který?",
+        "Krusty" : "V seriálu vystupuje i jeden klaun. Jak se jmenuje?"}
+
+def main():
+    odpoved = otazka_na_uzivatele()
+    for i in odpoved:
+        print(i)
+
+def otazka_na_uzivatele():
+    prvni_otazka = input(hra["žlutou"])
+    druhá_otázka = input(hra["saxofon"])
+    return prvni_otazka, druhá_otázka
+
+
+
+main()
+    
