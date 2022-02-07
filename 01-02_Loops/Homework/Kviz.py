770eddc8b16af9a4e2a778b591173a65c4da595b
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-02-07 18:44:56 +0100

    Kod dokončen

--- 01-02_Loops/Homework/Kviz.py
+++ 01-02_Loops/Homework/Kviz.py
@@ -9,21 +9,26 @@ from random import shuffle
 
 print('Vítej v kvízu o seriálu Simpsons')
 
-hra = {"žlutou" : "Všechny postavičky v tomto seriálu mají zvláštní barvu kůže. Jakou?",
-        "saxofon" : "Líza ráda hraje na jeden hudební nástroj. Na který?",
-        "Krusty" : "V seriálu vystupuje i jeden klaun. Jak se jmenuje?"}
+hra = {"Všechny postavičky v tomto seriálu mají zvláštní barvu kůže. Jakou?" : "žlutou",
+        "Líza ráda hraje na jeden hudební nástroj. Na který?" : "saxofon",
+        "V seriálu vystupuje i jeden klaun. Jak se jmenuje?" : "Krusty"}
 
-def main():
-    odpoved = otazka_na_uzivatele()
-    for i in odpoved:
-        print(i)
+dobre = 0
+spatne = 0
 
-def otazka_na_uzivatele():
-    prvni_otazka = input(hra["žlutou"])
-    druhá_otázka = input(hra["saxofon"])
-    return prvni_otazka, druhá_otázka
+def main(dobre, spatne):
+    for otazka in hra.keys():
+        odpoved_uzivatele = otazka_na_uzivatele(otazka)
+        if odpoved_uzivatele == hra[otazka]:
+            dobre += 1
+        elif odpoved_uzivatele != hra[otazka]:
+            spatne += 1
+    print('Máš tolik správně', dobre , 'a tolik špatně', spatne )
+
+def otazka_na_uzivatele(otazka):
+    return input(otazka)
+   
 
 
-
-main()
+main(dobre, spatne)
     
