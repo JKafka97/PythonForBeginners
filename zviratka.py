d290578d3354a6dc859bdc0c67b0c2389f1cfa06
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-02-07 20:38:17 +0100

    Přidání definic

--- zviratka.py
+++ zviratka.py
@@ -5,11 +5,30 @@
 # vysledek(parametr) jsou 4 býci
 # random.cislo   , proměné (krávy a bíci) budou slovník,   while(4býci),  for na projití všech číslovek,  indexování(+=1)    
 
+from random import randrange
 
+print('''Vítej u zábavné a všemi oblíbené hry KRAVY A BÝCI!
+    Tvým úkolem bude uhádnout čtyř místné číslo, které vygeneruje počítač. ''')
 
-
+dobytek = {"kráva" : 0, "býk" : 0}
 
 def main():
-    prdel
+    hodnota_pc = cislo_pc()
+    # # print(hodnota_pc)
+    # for i in hodnota_pc:
+        
 
+
+def typni_si():
+    return int(input("Jaký je tvůj typ? "))
+
+
+def cislo_pc():
+    return randrange(1000, 9999)
+
+
+
+
+
+main()
 
