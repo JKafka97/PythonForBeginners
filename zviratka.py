9fc87718ebf00164681a69d5b9386a44d96135bd
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-02-07 19:11:22 +0100

    DU- vytvoření zadání

---
+++ zviratka.py
@@ -0,0 +1,15 @@
+# krávy a býci 
+# random 4 ciferné číslo
+# ptaní se uživatele na 4 ciferné číslo
+# procházení čísla, když je na správném místě, tak je to býk. Když se tam vůbec nachází, tak je kráva
+# vysledek(parametr) jsou 4 býci
+# random.cislo   , proměné (krávy a bíci) budou slovník,   while(4býci),  for na projití všech číslovek,  indexování(+=1)    
+
+
+
+
+
+def main():
+    prdel
+
+
