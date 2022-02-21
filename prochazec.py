5e6af12c16f35ed5b39bad9aedf8d12c1c2098f8
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-02-21 18:44:07 +0100

    Přidání zadání

---
+++ prochazec.py
@@ -0,0 +1,4 @@
+# zadam minimálně do proměnné 200 slov(knížka) a on spočítá kolik je tam velkých písmen, malých písmen, čísel, ",", ".","!".
+# použiji for, držet to jako slovník
+# funkční login (3 uživatele)
+#
