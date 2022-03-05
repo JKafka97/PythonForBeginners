ed123acaa7f97c421b999a8994feb7ad9580e813
Sabina Koláčková <sabinakolackova@seznam.cz>
2022-03-05 18:32:38 +0100

    Počítá zatím jen velká A a B

--- prochazec.py
+++ prochazec.py
@@ -2,3 +2,20 @@
 # použiji for, držet to jako slovník
 # funkční login (3 uživatele)
 #
+
+slovnik = {"velká" : 0, "malá" : 0, "čísla" : 0, "čárky" : 0}
+
+
+def main():
+    text = otazka()
+    for i in text:
+        if i == 'A' or i == 'B':
+            slovnik["velká"]+=1
+    print('Tvůj text obsahuje: Velká písmena:', slovnik["velká"])
+
+
+def otazka():
+    return input('Vložte svůj text: ')
+
+
+main()
