1bba15668ff70ef86ff82486f33c507cc51a4f63
XFlexiX <kafkahonza1@gmail.com>
2022-03-15 18:26:49 +0100

    výpočet mzdy za měsíc

--- 01-01_Data_type/01-Penize.py
+++ 01-01_Data_type/01-Penize.py
@@ -2,7 +2,8 @@
 "řetězec jakýkoliv znáků" #string
 69420 #integer
 0.69 #float
-input() #zeptání se uživatele
-print() #vypsání do konzole
+# input() #zeptání se uživatele
+# print() #vypsání do konzole
 #(datové typy, vstup uživatele)
-
+otazka = int(input("Jaká je tvoje minutová mzda? "))
+print(otazka *60*8*22, "Peněz za měsíc")
