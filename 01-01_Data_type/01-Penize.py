63ad058ec3588a1f8378d144041534af9dadb47a
XFlexiX <kafkahonza1@gmail.com>
2022-03-15 18:07:08 +0100

    základní příkazy

--- 01-01_Data_type/01-Penize.py
+++ 01-01_Data_type/01-Penize.py
@@ -1,4 +1,8 @@
 #vypočti z minutové mzdy tu za měsíc
-
+"řetězec jakýkoliv znáků" #string
+69420 #integer
+0.69 #float
+input() #zeptání se uživatele
+print() #vypsání do konzole
 #(datové typy, vstup uživatele)
 
