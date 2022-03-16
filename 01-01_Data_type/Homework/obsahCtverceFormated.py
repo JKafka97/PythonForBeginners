6bf7726ccd4d5463a73670fc710b3f60027fee75
XFlexiX <kafkahonza1@gmail.com>
2022-03-16 21:23:16 +0100

    výpočet obsahu i s použitím .format

--- 01-01_Data_type/Homework/obsahCtverceFormated.py
+++ 01-01_Data_type/Homework/obsahCtverceFormated.py
@@ -1,3 +1,5 @@
 #zeptej se uživatele na parametry a vypočítej mu obsah čtvrce i s odpovědí (použíj .Format())
 
 #(.format())
+otazka = int (input("Jaké máš roměry čtverce v {m}? ".format(m = "m")))
+print("Obsah čverce je", otazka **2, "metrů čverečních")
