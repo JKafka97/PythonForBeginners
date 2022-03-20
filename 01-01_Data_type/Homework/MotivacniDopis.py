f36b09283a5cf2f9a16de40595886354bd4c6e64
XFlexiX <kafkahonza1@gmail.com>
2022-03-20 22:13:53 +0100

    MotivačníDopisLOL

--- 01-01_Data_type/Homework/MotivacniDopis.py
+++ 01-01_Data_type/Homework/MotivacniDopis.py
@@ -1,2 +1,25 @@
 #napiš motivační dopis s více použitím prom. pomocí .format()
+pozice = "Kvalita provozu"
+skola = "Univerzity Kralovy"
+vlastnosti = "spolehlivý, pečlivý, pracovitý a dochvělný"
+jmeno = "Mgr. Jan Kafka"
+a = """Vážená paní, vážený pane, 
 
+velice mě zaujala vámi nabízená pozice {0}, na stránce www.jobs.cz.
+Rád bych se připojili k vašemu týmu. 
+
+Jsem absolvent {1}, mám dlouho letou praxi v oboru s mnoha kurzy a
+certifikacemi. Podrobnosti najdete v mém životopisu.
+
+Rád pracuji v kolektivu a velice mě zajímají nové dovednosti a 
+zkušenosti. Jsem velice {2}.
+Chtěl bych u vás využít karierního růstu, stále se rozvíjet a 
+vylepšovat své znalosti.
+
+Velice se těším na naše setkání, kde bychom mohli probrat mé další
+možnosti které mi Vaše firma nabízí.
+
+S pozdravem
+
+{3} """.format(pozice, skola, vlastnosti, jmeno)
+print(a)
