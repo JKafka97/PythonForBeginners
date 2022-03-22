47716d58c0607251103b06d303c371ce71faae33
XFlexiX <kafkahonza1@gmail.com>
2022-03-22 20:59:37 +0100

    záloha

--- 01-01_Data_type/01-Penize.py
+++ 01-01_Data_type/01-Penize.py
@@ -2,8 +2,31 @@
 "řetězec jakýkoliv znáků" #string
 69420 #integer
 0.69 #float
+True, False #bool
 # input() #zeptání se uživatele
 # print() #vypsání do konzole
 #(datové typy, vstup uživatele)
 otazka = int(input("Jaká je tvoje minutová mzda? "))
 print(otazka *60*8*22, "Peněz za měsíc")
+x = str(float(int(str(5))))
+x = int(10.5)
+print(x)
+print(type(x))
+
+
+papu = "rohliku"
+apu = "ananas"
+a = """parek v {0} na {1}, 
+
+sdsdsdsdsd,
+
+sdsdsdsdsds """.format(papu, apu)
+print(a)
+
+if 1+1==2:
+    a = 5
+elif 1+1==2:
+    a = 6
+else:
+    a = 7
+print(a)
