f0f445dd4b7e7d9af2670e1aa0bb071acae74a2d
XFlexiX <kafkahonza1@gmail.com>
2022-03-27 19:09:30 +0200

    Boris má nápad

--- 01-01_Data_type/05-Princezna.py
+++ 01-01_Data_type/05-Princezna.py
@@ -1,1 +1,25 @@
-# vymysli kombinace pro hezkeho/ošklivého/hloupého/chytrého
+# vymysli kombinace pro hezkeho/ošklivého/hloupého/chytrého 
+def main():
+    krasa = otazka_krasa()
+    inteligence = otazka_inteligence()
+    shrnuti(krasa, inteligence)
+
+def otazka_krasa():
+    return input("Je princezna krasná? ")
+
+def otazka_inteligence():
+    return input("Je princezna chytrá? ")
+
+def shrnuti(krasa, inteligence):
+    if krasa=="ano" and inteligence=="ano":
+        print("Okeeeey lets go!!")
+    elif krasa=="ano" and inteligence=="ne":
+        print("Klasika čichna")
+    elif krasa=="ne" and inteligence=="ano":
+        print("FIZI")
+    elif krasa=="ne" and inteligence=="ne":
+        print("Asi Charbulka idk")
+
+main()
+
+
