a19c6ffb9509dddc0b7172510598b4593e613b2c
XFlexiX <kafkahonza1@gmail.com>
2022-03-27 18:41:05 +0200

    BMI kalkulátor

--- 01-01_Data_type/03-BMI.py
+++ 01-01_Data_type/03-BMI.py
@@ -1,2 +1,18 @@
 #vytvoř BMI kalkulačku 
 
+def main():
+    vaha = otazka_vaha()
+    vyska = otazka_vyska()
+    vypocet(vaha, vyska)
+
+def otazka_vaha():
+    a = int(input("Jaká je tvoje váha? "))
+    return a
+
+def otazka_vyska():
+    return float(input("Jaká je tvoje výška? "))
+
+def vypocet(vaha, vyska):
+    print(vaha/vyska**2)
+
+main()
