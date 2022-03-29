fe7f5b470cd7ebc40ce44debad1cc93d8ff7bc92
XFlexiX <kafkahonza1@gmail.com>
2022-03-29 19:13:35 +0200

    CestaDoLesa

--- 01-02_Loops/01-CestaDoLesa.py
+++ 01-02_Loops/01-CestaDoLesa.py
@@ -1,1 +1,29 @@
 #pomocí vlastních metod nadefinuj kroky na cestu do lesa (def + while)
+
+def main():
+    while True:
+        otazka = kam_jit()
+        if otazka == "doleva":
+            doleva()
+        elif otazka == "doprava":
+            doprava()
+        elif otazka == "dopredu":
+            dopredu()
+        elif otazka == "konec":
+            break
+        else:
+            print("Jdi v prdel")
+
+def doleva():
+    print("Krok doleva")
+
+def doprava():
+    print("Krok doprava")
+
+def dopredu():
+    print("Krok dopředu")
+
+def kam_jit():
+    return input("Kam mám jít? ")
+
+main()
