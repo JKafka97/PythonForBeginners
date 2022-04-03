57b2d1b6d89dc17bb3ba3f226ca625095ca597cc
XFlexiX <kafkahonza1@gmail.com>
2022-04-03 19:21:23 +0200

    RandomCislo

--- 01-02_Loops/02-RandomCislo.py
+++ 01-02_Loops/02-RandomCislo.py
@@ -1,2 +1,20 @@
 #Typni si číslo od 1 do 5! (while + random)
 #hra si jednou tipne číslo, opakovaně se na číslo bude ptát
+
+from random import randint
+
+def main():
+    uzivatel_tip = cislo_uzivatel()
+    pocitac_tip = cislo_pocitac()
+    while uzivatel_tip != pocitac_tip:
+        print("Špatný tip zkus znovu.")
+        uzivatel_tip = cislo_uzivatel()
+    print("Supr")
+
+def cislo_uzivatel():
+    return int(input("Tipni si číslo: "))
+
+def cislo_pocitac():
+    return randint(1, 5)
+
+main()
