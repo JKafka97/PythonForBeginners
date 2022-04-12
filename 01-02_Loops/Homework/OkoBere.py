658552b354c7a4ef77c3ba3c1c475b98f0901d0d
XFlexiX <kafkahonza1@gmail.com>
2022-04-12 20:07:17 +0200

    OkoBere

--- 01-02_Loops/Homework/OkoBere.py
+++ 01-02_Loops/Homework/OkoBere.py
@@ -6,3 +6,36 @@
 # když víc => vypíšu o kolik a že prohrál
 
 #(while + random)
+
+from random import randint
+
+def main():
+  otoceni = "ano"
+  hrac, pocitac = 0,0
+  while hrac < 21 and otoceni == "ano":
+    hrac += otaceni_karty()
+    pocitac += otaceni_karty()
+    print("Ty máš", hrac, "Počítač má", pocitac)
+    otoceni = otazka_otoceni_karty()
+   
+  if hrac == 21:
+    print("Jsi výherce.")
+  elif hrac > 21:
+    print("Prohra.")
+  elif pocitac > 21:
+    print("Výhra.")
+  else:
+    if hrac > pocitac:
+      print("Výhra.")
+    elif pocitac > hrac:
+      print("Prohra.")
+    else:
+      print("Vyrovnané.")
+   
+def otaceni_karty():
+  return randint(2, 10)
+
+def otazka_otoceni_karty():
+  return input("Chceš otočit další kartu? ")
+
+main()
