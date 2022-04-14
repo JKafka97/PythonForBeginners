7ceff562e2500076d37b5146e36d1bd347490f9b
XFlexiX <kafkahonza1@gmail.com>
2022-04-14 20:52:38 +0200

    KamenNuzkyPapir

--- 01-02_Loops/Homework/KamenNuzkyPapir.py
+++ 01-02_Loops/Homework/KamenNuzkyPapir.py
@@ -1,2 +1,25 @@
 # pomocí listu udělej kámen nužky papir (list + while)
 
+import random
+
+def main():
+    uzivatel = volba_uzivatele()
+    pocitac = volba_pocitace()
+    vyhodnoceni(uzivatel, pocitac)
+    
+def volba_pocitace():
+    list = ["kámen", "nůžky", "papír"]
+    print(random.choice(list))
+    
+def volba_uzivatele():
+    return input("Kámen, nůžky nebo papír? ")
+
+def vyhodnoceni(uzivatel, pocitac):
+    if uzivatel == "kámen" and pocitac=="nůžky" or uzivatel == "nůžky" and pocitac == "papír" or uzivatel == "papír" and pocitac == "kámen":
+        print("Vyhral jsi!")
+    elif uzivatel == "kámen" and pocitac == "kámen" or uzivatel == "nůžky" and pocitac == "nůžky" or uzivatel == "papír" and pocitac == "papír": 
+        print("Plichta")
+    elif uzivatel == "kámen" and pocitac == "papír" or uzivatel == "nůžky" and pocitac == "kámen" or uzivatel == "papír" and pocitac == "nůžky":
+        print("Prohrál jsi")
+
+main()
